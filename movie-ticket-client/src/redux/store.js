3a508c5aba392a5bda565f81359dd11011d6d33d
josephsamimmanuel <josesamimmanuel@gmail.com>
2025-04-06 16:50:46 +0530

    Fix import case sensitivity for loaderReducer in Redux store configuration.

--- movie-ticket-client/src/redux/store.js
+++ movie-ticket-client/src/redux/store.js
@@ -1,5 +1,5 @@
 import {configureStore} from "@reduxjs/toolkit";
-import loaderReducer from "./loaderSlice";
+import loaderReducer from "./loaderslice";
 import usersReducer from "./usersSlice";
 import moviesReducer from "./moviesSlice";
 import theatreListReducer from "./theatreListSlice";
